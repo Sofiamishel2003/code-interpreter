74a3f399bbb7692393cfc42933a86786ca3bd9d6
sofiaWebT <98661058+Sofiamishel2003@users.noreply.github.com>
2024-11-15 10:30:23 -0600

    Final commit on second part

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from langchain_experimental.tools import PythonREPLTool
 
 load_dotenv()
 
+
 def main():
     print("Start...")
 
@@ -29,12 +30,33 @@ def main():
         tools=tools,
     )
     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    agent_executor.invoke(
+
+    # agent_executor.invoke(
+    #     input={
+    #         "input": """generate and save in current working directory 2 QR codes that point to www.google.com,
+    #         you have qrcode package installed already."""
+    #     }
+    # )
+
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
+        allow_dangerous_code=True,
+    )
+
+    # csv_agent.invoke(
+    #    input={
+    #        "input": "how many columns are there in the file episode_info.csv?"
+    #    }
+    # )
+
+    csv_agent.invoke(
         input={
-            "input": """generate and save in current working directory 2 QR codes that point to www.google.com,
-            you have qrcode package installed already."""
+            "input": "how many episodes did Larry David write?"
         }
     )
+
 
 if __name__ == "__main__":
     main()
